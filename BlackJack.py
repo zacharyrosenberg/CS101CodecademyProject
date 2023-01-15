8ac7ee69300cc57bd02c382513659837db72b00e
Zack Rosenberg <60266323+zacharyrosenberg@users.noreply.github.com>
2023-01-15 12:55:06 -0500

    Added Dealer class and added deal cards method to dealer class

--- BlackJack.py
+++ BlackJack.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ['spade', 'club', 'heart', 'diamond']
 values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 class Cards:
@@ -8,10 +10,21 @@ class Cards:
     def __repr__(self):
         return '({value}, {suit})'.format(value = self.value, suit = self.suit)
 
+class Dealer(Cards):
+    def __init__(self, hand = []):
+        self.hand = hand
+    
+    def dealCards(self, hand):
+        if len(self.hand) == 0:
+            self.hand.append(random.choices(deck, k = 2))
+        return self.hand
 
+deck = [Cards(value, suit) for value in values for suit in suits]  
+print(deck)
+casino = Dealer()
+casino.dealCards(hand = [])
+print(casino.hand)
 
-Deck = [Cards(value, suit) for value in values for suit in suits]  
-print(Deck)
 
 
 
