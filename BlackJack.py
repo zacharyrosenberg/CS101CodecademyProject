504037bb5d495aabe5b2eb0887505a375b38f0ea
Zack Rosenberg <60266323+zacharyrosenberg@users.noreply.github.com>
2023-01-17 18:20:55 -0500

    Added dealer total instance variable to check when dealer hits 17

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,9 @@
 import random
 
 suits = ['spade', 'club', 'heart', 'diamond']
-values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
+nameToValue = {'jack': 10, 'queen': 10, 'king': 10, 'ace': 11}
+playerHand = []
 class Cards:
     def  __init__(self, value, suit):
         self.value = value
@@ -13,17 +15,40 @@ class Cards:
 class Dealer(Cards):
     def __init__(self, hand = []):
         self.hand = hand
+        self.total = 0
     
     def dealCards(self, hand):
         if len(self.hand) == 0:
-            self.hand.append(random.choices(deck, k = 2))
-        return self.hand
+            for i in range(0,2):
+                self.hand.append(deck.pop(random.randrange(len(deck))))
+                playerHand.append(deck.pop(random.randrange(len(deck))))
+        for cards in self.hand:
+            if cards.value in nameToValue:
+                self.total += nameToValue[cards.value]
+            else:
+                self.total += cards.value
+            
+    def dealerHit(self):
+        while self.total < 17:
+            self.hand.append(deck.pop(random.randrange(len(deck))))
+            self.total += self.hand.value
+        
+
+
+        
+        
+        
+
 
 deck = [Cards(value, suit) for value in values for suit in suits]  
-print(deck)
+#print(deck)
 casino = Dealer()
 casino.dealCards(hand = [])
+casino.dealerHit()
 print(casino.hand)
+#print(playerHand)
+#print(deck)
+print(casino.total)
 
 
 
