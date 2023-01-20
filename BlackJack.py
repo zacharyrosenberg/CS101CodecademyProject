7a68d7ce949cfc0133fd462fcf2334f4c5dda58c
Zack Rosenberg <60266323+zacharyrosenberg@users.noreply.github.com>
2023-01-20 18:13:07 -0500

    Updated class methods and game loop
    - Fixed counting bugs
    - Updated play and dealerHit methods
    - Updated game function and game loop

--- BlackJack.py
+++ BlackJack.py
@@ -3,8 +3,7 @@ import random
 suits = ['spade', 'club', 'heart', 'diamond']
 values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
 nameToValue = {2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'jack': 10, 'queen': 10, 'king': 10, 'ace': 11}
-playerHand = []
-playerTotal = 0
+
 
 class Cards:
     def  __init__(self, value, suit):
@@ -21,82 +20,94 @@ class Dealer(Cards):
     
     def dealCards(self):
         for i in range(0,2):
+            person1.hand.append(deck.pop(random.randrange(len(deck))))
+        for i in range(0,1):
             self.hand.append(deck.pop(random.randrange(len(deck))))
-            playerHand.append(deck.pop(random.randrange(len(deck))))
         for cards in self.hand:
             self.total += nameToValue[cards.value]
-    
-
+        for cards in person1.hand:
+            person1.total += nameToValue[cards.value]
 
     def dealerHit(self):
         while self.total < 17:
             self.hand.append(deck.pop(random.randrange(len(deck))))
-            for cards in self.hand:
-                if cards.value in nameToValue:
-                    self.total += nameToValue[cards.value]
-                else:
-                    self.total += cards.value
+            self.total += nameToValue[self.hand[-1].value]
+        print(str(self.hand) + ' casino hand')
         if self.total > 21:
-            print('The dealer went bust! You win $')
-        
+            print('The dealer went bust! You win $' + str(person1.money))
+        elif self.total > person1.total:
+            print('The dealer wins. You lose $' + str(person1.money) + ' :(.')
+        elif self.total < person1.total:
+            print('You win $' + str(person1.money) + '!')
+        elif self.total == person1.total:
+            print('You push!')
+        else:
+            pass
+
+                
 class Player(Cards):
     def __init__(self):
-        self.hand = playerHand
-        self.total = playerTotal
-        self.money = int(input('How much money do you want to bet?: '))
+        self.hand = []
+        self.total = 0
+        self.money = int(input('Welcome to Blackjack with Chuck! How much money do you want to bet?: '))
 
     def play(self):
-        playerInput = input('Do you want to hit, stand, double down, or split?: ')
-        if playerInput == 'hit' or playerInput == 'Hit':
-            self.hand.append(deck.pop(random.randrange(len(deck))))
-            if self.total > 22:
-                print('You went over 21, you lose.')
-            else:
-                pass
-        elif playerInput == 'stand' or playerInput == 'Stand':
-            pass
-        elif playerInput == 'double down' or playerInput == 'Double Down':
-            self.money = self.money * 2 
+        global gameOn
+        while gameOn:
+            playerInput = input('Do you want to hit, stand, double down, or split?: ')
+            if playerInput == 'hit' or playerInput == 'Hit':
+                self.hand.append(deck.pop(random.randrange(len(deck))))
+                self.total += nameToValue[self.hand[-1].value]
+                print(str(person1.hand) + ' your hand')
+                if self.total > 22:
+                    print('You went over 21, you lose $' + str(self.money) + '.')
+                    gameOn = False
+                else:
+                    pass
+            elif playerInput == 'stand' or playerInput == 'Stand':
+                print('You stand.')
+                gameOn = False
+            elif playerInput == 'double down' or playerInput == 'Double Down':
+                print('You doubled you bet to $' + str(self.money * 2) + '.')
+                self.money = self.money * 2 
+                gameOn = False
         
-
-###The dealerHit and play methods are bugged. Also, when you play again after already playing a hand, the hands do not reset
-
-        
-
-
-
+def game():
+    casino.dealCards()
+    print(str(casino.hand) + ' casino hand')
+    print(str(person1.hand) + ' your hand')
+    person1.play()
+    casino.dealerHit()
+    question = input('Do you want to play again?: ')
+    if question == 'Yes' or question == 'yes':
+        person1.hand = []
+        person1.total = 0
+        casino.hand = []
+        casino.total = 0
+        deck = [Cards(value, suit) for value in values for suit in suits]
+        game()
+    else:
+        print('Have a nice day!')
+        gameOn = False
+    
+deck = [Cards(value, suit) for value in values for suit in suits]
 casino = Dealer()
 person1 = Player()
 gameOn = True
-deck = [Cards(value, suit) for value in values for suit in suits]
-
-
-def game():
-    casino.dealCards()
-    #casino.dealerHit()
-    print(str(casino.hand) + ' casino hand')
-    print(str(playerHand) + ' your hand')
-    print(casino.total)
-    print(person1.total)
-    person1.play()
-
-
-
-
 
 while gameOn:
-    print('Welcome to Blackjack! Do you want to play?')
-    userInput = input('Yes or No: ')
-    if userInput == 'Yes' or userInput == 'yes':
-       game()
-    else:
-        print('So long and have a nice day!')
-        gameOn = False
+    game()
         
 
     
+### KNOWN BUGS: - Saying yes to play again doesnt ask for user input.
+### - Must add blackjack feature (ie dealt 21)
+### - If I go over 21 I lose money, but my total is higher than casino, so it prints I win as well(dealer hit method)
+
 
   
+
+
 #print(deck)
 
 #casino.dealCards(hand = [])
